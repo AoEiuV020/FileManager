06fc9fc965803b6f7e0c98956bd3b020244512f9
AoEiuV-PC <490674483@qq.com>
2016-04-24 05:01:40 +0800

    回炉重造
    显示模仿ES文件管理器，
    功能暂时只有打开文件，
    有多选的框框，
    但是没用，

---
+++ src/com/aoeiuv020/filemanager/ItemAdapter.java
@@ -0,0 +1,224 @@
+/* ***************************************************
+	^> File Name: ItemAdapter.java
+	^> Author: AoEiuV020
+	^> Mail: 490674483@qq.com
+	^> Created Time: 2016/04/21 - 23:44:33
+*************************************************** */
+package com.aoeiuv020.filemanager;
+import com.aoeiuv020.tool.Logger;
+import android.widget.*;
+import android.content.*;
+import android.view.*;
+import android.os.*;
+import java.util.*;
+import java.io.*;
+import java.text.SimpleDateFormat;
+class ItemAdapter extends BaseAdapter
+{
+	List<Item> mList=null;
+	private Context mContext=null;
+	private LayoutInflater mInflater=null;
+	private Comparator mComparator=MyComparator.NameASC;
+	private static final FilenameFilter filter=new MyFilenameFilter();
+	public ItemAdapter(Context context)
+	{
+		mList=new LinkedList<Item>();
+		mContext=context;
+		mInflater=LayoutInflater.from(context);
+	}
+	public void set(File file)
+	{
+		mList.clear();
+		addAll(file.listFiles(filter));
+		Collections.sort(mList,mComparator);
+		notifyDataSetChanged();
+	}
+	private void add(File file)
+	{
+		Item item=new Item();
+		item.file=file;
+		mList.add(item);
+	}
+	private void addAll(File[] list)
+	{
+		if(list==null||list.length==0)
+			return;
+		for(File file:list)
+		{
+			add(file);
+		}
+	}
+	public void addAll(Collection<File> list)
+	{
+		if(list==null||list.size()==0)
+			return;
+		for(File file:list)
+		{
+			add(file);
+		}
+		notifyDataSetChanged();
+	}
+	@Override
+	public int getCount()
+	{
+		return mList.size();
+	}
+	@Override
+	public Object getItem(int position)
+	{
+		return mList.get(position);
+	}
+	@Override
+	public long getItemId(int position)
+	{
+		return position;
+	}
+	@Override
+	public View getView(int position,View convertView,ViewGroup parent)
+	{
+		View view=null;
+		ViewHolder holder=null;
+		if(convertView==null)
+		{
+			holder=new ViewHolder();
+			view=ViewHolder.inflate(mInflater);
+			holder.find(view);
+			view.setTag(holder);
+		}
+		else
+		{
+			view=convertView;
+			holder=(ViewHolder)view.getTag();
+		}
+		Item item=(Item)mList.get(position);
+		holder.set(item);
+		return view;
+	}
+	public static class MyComparator
+	{
+		public static final Comparator<Item> NameASC=new NameComparator(){
+				@Override
+				public int compare(String nameA,String nameB)
+				{
+					return nameA.compareTo(nameB);
+				}
+			};
+		public static final Comparator<Item> NameDESC=new NameComparator(){
+				@Override
+				public int compare(String nameA,String nameB)
+				{
+					return -nameA.compareTo(nameB);
+				}
+			};
+		public static final Comparator<Item> TimeASC=null;
+		public static final Comparator<Item> TimeDESC=null;
+		public static final Comparator<Item> SizeASC=null;
+		public static final Comparator<Item> SizeDESC=null;
+		private static abstract class NameComparator implements Comparator<Item>
+		{
+			@Override
+			public int compare(Item a,Item b)
+			{
+				if(a.file==b.file)
+					return 0;
+				if(a.file.isDirectory()&&!b.file.isDirectory())
+					return Integer.MIN_VALUE;
+				if(!a.file.isDirectory()&&b.file.isDirectory())
+					return Integer.MAX_VALUE;
+				String nameA=null,nameB=null;
+				if(a.file!=null)
+					nameA=a.file.getName();
+				if(b.file!=null)
+					nameB=b.file.getName();
+				return compare(nameA,nameB);
+			}
+			public abstract int compare(String nameA,String nameB);
+		}
+	}
+	public static class Item implements Comparable<Item>
+	{
+		public File file=null;
+		public boolean selected=false;
+		@Override
+		public int compareTo(Item item)
+		{
+			return MyComparator.NameASC.compare(this,item);
+		}
+		@Override
+		public String toString()
+		{
+			return String.format("<%d,%s,%s>",hashCode(),file==null?file:file.getName(),selected);
+		}
+	}
+	public static class ViewHolder
+	{
+		private static final int ICON_FOLDER=R.drawable.format_folder;
+		private static final int ICON_FILE=R.drawable.format_unknown;
+		private static final SimpleDateFormat sdformat=new SimpleDateFormat("yyyy/MM/dd-HH:mm:ss");
+		public ImageView image=null;
+		public TextView name=null;
+		public TextView size=null;
+		public TextView permission=null;
+		public TextView time=null;
+		public CheckBox selected=null;
+		public static View inflate(LayoutInflater inflater)
+		{
+			return inflater.inflate(R.layout.layout_item,null);
+		}
+		public void set(Item item)
+		{
+			if(item==null||item.file==null)
+				return;
+			File file=item.file;
+			int resId=ICON_FILE;
+			if(file.isDirectory())
+				resId=ICON_FOLDER;
+			image.setImageResource(resId);
+			name.setText(""+file.getName());
+			size.setText(""+file.length());
+			permission.setText(""+(file.isDirectory()?"d":"-")+(file.canRead()?"r":"-")+(file.canWrite()?"w":"-")+(file.canExecute()?"x":"-"));
+			time.setText(""+sdformat.format(new Date(file.lastModified())));
+			Logger.v("ViewHolder set %s",item);
+			selected.setOnCheckedChangeListener(new SelectListener(item));
+			selected.setChecked(item.selected);
+		}
+		public void find(View view)
+		{
+			if(view==null)
+				return;
+			image=(ImageView)view.findViewById(R.id.item_image);
+			name=(TextView)view.findViewById(R.id.item_name);
+			size=(TextView)view.findViewById(R.id.item_size);
+			permission=(TextView)view.findViewById(R.id.item_permission);
+			time=(TextView)view.findViewById(R.id.item_time);
+			selected=(CheckBox)view.findViewById(R.id.item_selected);
+		}
+		private class SelectListener implements CompoundButton.OnCheckedChangeListener
+		{
+			private Item mItem=null;
+			public SelectListener(Item item)
+			{
+				mItem=item;
+			}
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView,boolean isChecked)
+			{
+				Logger.v("onCheckedChanged ,%s,%s",mItem,isChecked);
+				mItem.selected=isChecked;
+			}
+		}
+	}
+}
+class MyFilenameFilter implements FilenameFilter
+{
+	@Override
+	public boolean accept(File dir,String filename)
+	{
+		boolean result=true;
+		if(filename.equals("."))
+			result=false;
+		else if(filename.equals(".."))
+			result=false;
+		return result;
+	}
+}
--- src/com/aoeiuv020/filemanager/Main.java
+++ src/com/aoeiuv020/filemanager/Main.java
@@ -1,9 +1,12 @@
 package com.aoeiuv020.filemanager;
+import com.aoeiuv020.tool.Logger;
+import com.aoeiuv020.widget.SimpleToast;
 
 import android.app.Activity;
-import android.os.Bundle;
+import android.os.*;
 import android.widget.*;
 import android.view.*;
+import android.net.*;
 import android.content.*;
 import android.util.Log;
 import java.util.*;
@@ -14,143 +17,78 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 
 
-public class Main extends Activity implements AdapterView.OnItemClickListener,AdapterView.OnItemLongClickListener
+public class Main extends Activity implements AdapterView.OnItemClickListener
 {
 	private final String TAG=""+this;
-	private final int ICON_FOLDER=android.R.drawable.sym_contact_card;
-	private final int ICON_FILE=android.R.drawable.star_on;
-	private List<File> fileList=null;
-	private final String ROOT_PATH="/";
+	private ListView lvFileList=null;
+	private File currentFile=null;
+	private ItemAdapter mAdapter=null;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
-        super.onCreate(savedInstanceState);
-		show(ROOT_PATH);
-    }
-	void setFileList(String string)
-	{
-		File file=new File(string);
-		setFileList(file);
-	}
-	void setFileList(File file)
-	{
-		fileList=new ArrayList<File>();
-		if(file==null)
-		{
-			return;
-		}
-		if(file.isDirectory())
-		{
-			File parent=file.getParentFile();
-			if(parent!=null)
-			{
-				fileList.add(parent);
-			}
-			else
-			{
-				fileList.add(file);
-			}
-			fileList.addAll(Arrays.asList(file.listFiles()));
-		}
-		else
-		{
-			//正常不会到这里，
-			Log.w(TAG,"fileList only one file");
-			fileList.add(file);
-		}
-	}
-	void show(String string)
-	{
-		File file=new File(string);
-		show(file);
-	}
-	void show(File directory)
-	{
-		List<Map<String,Object>> list=new LinkedList<Map<String,Object>>();
-		Map<String,Object> map=null;
-		setFileList(directory);
-		int i=0;
-		for(File file:fileList)
-		{
-			map=new HashMap<String,Object>();
-			if(file.isDirectory())
-			{
-				map.put("image",ICON_FOLDER);
-			}
-			else
-			{
-				map.put("image",ICON_FILE);
-			}
-			++i;
-			if(i==1)
-			{
-				map.put("text","..");
-			}
-			else
-			{
-				map.put("text",file.getName());
-			}
-			list.add(map);
-		}
-		String[] from={"image","text"};
-		int[] to={R.id.image,R.id.text};
-		ListAdapter adapter=new SimpleAdapter(this,list,R.layout.main,from,to);
-		ListView listView=new ListView(this);
-		listView.setAdapter(adapter);
-		listView.setOnItemClickListener(this);
-		listView.setOnItemLongClickListener(this);
-		setContentView(listView);
-	}
-	void resetContentView(File file)
-	{
-		show(file);
-	}
-	@Override
-	public boolean onItemLongClick(AdapterView<?> parent,View view,int position,long id)
-	{
-		final File file=fileList.get(position);
-		final EditText editText=new EditText(this);
-		AlertDialog.Builder builder=new AlertDialog.Builder(this);
-		builder.setIcon(android.R.drawable.ic_dialog_info);
-		builder.setView(editText);
-		builder.setTitle("输入拓展名");
-		builder.setPositiveButton("OK",new DialogInterface.OnClickListener(){
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.main);
+		lvFileList=(ListView)findViewById(R.id.filelist);
+		LayoutInflater inflater=LayoutInflater.from(this);
+		View parent=inflater.inflate(R.layout.layout_parent,null);
+		parent.setOnClickListener(new View.OnClickListener(){
 			@Override
-			public void onClick(DialogInterface dialog,int id)
+			public void onClick(View view)
 			{
-				String extension=""+editText.getText();
-				String type=MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
-				Intent intent=new Intent();
-				intent.setAction(Intent.ACTION_VIEW);
-				intent.setDataAndType(Uri.fromFile(file),""+type);
-				try
-				{
-					startActivity(intent);
-				}
-				catch(ActivityNotFoundException e)
-				{
-					Log.w(Main.this.TAG,""+extension+","+type);
-				}
+				Main.this.backParent();
 			}
 		});
-		builder.show();
-		return true;
+		lvFileList.addHeaderView(parent);
+		currentFile=Environment.getExternalStorageDirectory();
+		mAdapter=new ItemAdapter(this);
+		mAdapter.set(currentFile);
+		lvFileList.setAdapter(mAdapter);
+		lvFileList.setOnItemClickListener(this);
+    }
+	public void backParent()
+	{
+		File parent=currentFile.getParentFile();
+		if(parent!=null)
+			currentFile=parent;
+		mAdapter.set(currentFile);
 	}
 	@Override
 	public void onItemClick(AdapterView<?> parent,View view,int position,long id)
 	{
-		File file=fileList.get(position);
+		ItemAdapter.Item item=(ItemAdapter.Item)parent.getAdapter().getItem(position);
+		File file=item.file;
+		Logger.v("onItemClick %d,%s",position,file.getPath());
+		if(file==null)
+		{
+			//不可到达,
+			Logger.e(new Exception("Item"+item+"里没file"));
+		}
 		if(file.isDirectory())
 		{
-			resetContentView(file);
+			currentFile=file;
+			ItemAdapter adapter=null;
+			//有Header，这里永真,
+			if(parent.getAdapter() instanceof HeaderViewListAdapter)
+			{
+				adapter=(ItemAdapter)((HeaderViewListAdapter)parent.getAdapter()).getWrappedAdapter();
+			}
+			else
+				adapter=(ItemAdapter)parent.getAdapter();
+			adapter.set(file);
 		}
 		else
 		{
-			String extension=getExtension(file);
-			String type=MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
 			Intent intent=new Intent();
 			intent.setAction(Intent.ACTION_VIEW);
+			String extension=file.getName();
+			int lastDot=extension.lastIndexOf('.');
+			if(lastDot!=-1)
+			{
+				extension=extension.substring(lastDot+1);
+			}
+			String type=MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
 			intent.setDataAndType(Uri.fromFile(file),type);
 			try
 			{
@@ -158,26 +96,21 @@ public class Main extends Activity implements AdapterView.OnItemClickListener,Ad
 			}
 			catch(ActivityNotFoundException e)
 			{
-				Log.w(Main.this.TAG,""+extension+","+type);
+				SimpleToast.makeText(this,String.format("没有应用可以打开文件%s,后辍%s,类型%s",file.getName(),extension,type));
 			}
 		}
 	}
-	String getExtension(File file)
+	private long lastTime=0;
+	@Override
+	public void onBackPressed()
 	{
-		if(file!=null)
+		long currentTime=System.currentTimeMillis();
+		if(currentTime-lastTime<1*1000)
+			finish();
+		else
 		{
-			return getExtension(file.getName());
+			lastTime=currentTime;
+			backParent();
 		}
-		return "";
-	}
-	String getExtension(String fileName)
-	{
-		String extension=fileName;
-		int lastDot=fileName.lastIndexOf('.');
-		if(lastDot!=-1)
-		{
-			extension=fileName.substring(lastDot+1);
-		}
-		return extension;
 	}
 }
